eb118ff1e4e43c5c8051b18041a518252bde0980
Kevyn Marins <kevynmarins@gmail.com>
2021-01-12 03:04:55 +0000

    add constructor to the ClientClass

--- src/D3/models/ClientClass.java
+++ src/D3/models/ClientClass.java
@@ -5,10 +5,15 @@ import java.util.List;
 
 public class ClientClass implements Client{
     private String name;
-    private Employee manager = new ManagerClass();
+    private int idManager;
     private List<Item> inventory = new LinkedList<Item>();
     private List<Deposit> deposits = new LinkedList<Deposit>();
     private List<Delivery> deliveries = new LinkedList<Delivery>();
+
+    public ClientClass(String name, int identificadorFuncionario) {
+        this.name = name;
+        this.idManager = identificadorFuncionario;
+    }
 
     @Override
     public String get_name() {
